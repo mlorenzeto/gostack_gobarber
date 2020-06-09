195af8fe7e9cd97b526ee074254701ebd870a7f4
Matheus Lorenzeto <matheus.mlorenzeto@gmail.com>
2020-06-08 21:16:02 -0300

    add notifications module, integrate it with the appointments creation and configure the Mongo DB to save the notifications

---
+++ backend/src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import ListProviderAppointmentsService from '@modules/appointments/services/ListProviderAppointmentsService';
+
+export default class ProviderAppointmentsController {
+  public async index(request: Request, response: Response): Promise<Response> {
+    const { day, month, year } = request.body;
+    const provider_id = request.user.id;
+
+    const listProviderAppointment = container.resolve(
+      ListProviderAppointmentsService,
+    );
+
+    const appointments = await listProviderAppointment.execute({
+      provider_id,
+      year,
+      month,
+      day,
+    });
+
+    return response.json(appointments);
+  }
+}
---
+++ backend/src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -0,0 +1,44 @@
+import { injectable, inject } from 'tsyringe';
+
+import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
+import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
+
+interface IRequest {
+  provider_id: string;
+  month: number;
+  year: number;
+  day: number;
+}
+
+type IReponse = Array<{
+  day: number;
+  available: boolean;
+}>;
+
+@injectable()
+class ListProviderAppointmentsService {
+  constructor(
+    @inject('AppointmentsRepository')
+    private appointmentsRepository: IAppointmentsRepository,
+  ) {}
+
+  public async execute({
+    provider_id,
+    year,
+    month,
+    day,
+  }: IRequest): Promise<Appointment[]> {
+    const appointments = await this.appointmentsRepository.findAllInDayFromProvider(
+      {
+        provider_id,
+        year,
+        month,
+        day,
+      },
+    );
+
+    return appointments;
+  }
+}
+
+export default ListProviderAppointmentsService;
